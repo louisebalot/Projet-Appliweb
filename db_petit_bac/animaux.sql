fc9361c9a34e83410cee5a6ca8bafcb086a726d5
lbt3875 <lbt3875@griffon.enseeiht.fr>
2026-04-03 16:02:21 +0200

    bdd

---
+++ db_petit_bac/animaux.sql
@@ -0,0 +1,29 @@
+CREATE TABLE IF NOT EXISTS table_animaux (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL
+);
+
+INSERT INTO table_animaux (nom) VALUES 
+('Abeille'), ('Aigle'), ('Albatros'), ('Alligator'), ('Alpaga'), ('Âne'), ('Antilope'), 
+('Araignée'), ('Autruche'), ('Baleine'), ('Belette'), ('Béluga'), ('Bison'), ('Blaireau'), 
+('Boa'), ('Bœuf'), ('Bouquetin'), ('Bourdon'), ('Buffle'), ('Cachalot'), ('Caméléon'), 
+('Campagnol'), ('Canard'), ('Capybara'), ('Caracal'), ('Caribou'), ('Castor'), ('Cerf'), 
+('Chacal'), ('Chameau'), ('Chamois'), ('Chat'), ('Chauve-souris'), ('Chenille'), ('Cheval'), 
+('Chevreuil'), ('Chèvre'), ('Chien'), ('Chimpanzé'), ('Chinchilla'), ('Chouette'), ('Cigogne'), 
+('Cobra'), ('Cochon'), ('Cochon d''Inde'), ('Coccinelle'), ('Colibri'), ('Condor'), ('Coyote'), 
+('Crabe'), ('Crapaud'), ('Crevette'), ('Crocodile'), ('Cygne'), ('Dauphin'), ('Daim'), 
+('Dindon'), ('Dromadaire'), ('Écureuil'), ('Éléphant'), ('Élan'), ('Épervier'), ('Escargot'), 
+('Espadon'), ('Faisan'), ('Faon'), ('Faucon'), ('Fennec'), ('Flamant rose'), ('Fouine'), 
+('Fourmi'), ('Furet'), ('Gazelle'), ('Girafe'), ('Gorille'), ('Grenouille'), ('Guépard'), 
+('Guêpe'), ('Hamster'), ('Hérisson'), ('Héron'), ('Hibou'), ('Hippopotame'), ('Hirondelle'), 
+('Hyène'), ('Iguane'), ('Isard'), ('Jaguar'), ('Kangourou'), ('Koala'), ('Lama'), ('Lapin'), 
+('Léopard'), ('Lézard'), ('Libellule'), ('Lièvre'), ('Lion'), ('Loir'), ('Loup'), ('Loutre'), 
+('Lycaon'), ('Lynx'), ('Marmotte'), ('Manchot'), ('Méduse'), ('Mésange'), ('Morse'), ('Mouche'), 
+('Moustique'), ('Mouton'), ('Mulet'), ('Musaraigne'), ('Narval'), ('Okapi'), ('Oie'), ('Opossum'), 
+('Orque'), ('Ornithorynque'), ('Oryx'), ('Ours'), ('Ours polaire'), ('Panda'), ('Panthère'), 
+('Paon'), ('Paresseux'), ('Perroquet'), ('Perruche'), ('Phoque'), ('Pie'), ('Pieuvre'), 
+('Pigeon'), ('Pingouin'), ('Poney'), ('Porc-épic'), ('Poule'), ('Puma'), ('Putois'), 
+('Python'), ('Rat'), ('Raton laveur'), ('Renard'), ('Renne'), ('Requin'), ('Rhinocéros'), 
+('Salamandre'), ('Sanglier'), ('Sauterelle'), ('Saumon'), ('Singe'), ('Souris'), ('Suricate'), 
+('Taupe'), ('Taureau'), ('Tigre'), ('Tortue'), ('Toucan'), ('Vache'), ('Vautour'), ('Vipère'), 
+('Zèbre'), ('Zébu');
