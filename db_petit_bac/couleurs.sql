da4ec31903ec5aa5d1ba52155b8e6a2662a03338
lbt3875 <lbt3875@griffon.enseeiht.fr>
2026-04-03 15:21:46 +0200

    bdd

---
+++ db_petit_bac/couleurs.sql
@@ -0,0 +1,36 @@
+-- 1. Création de la table simplifiée
+CREATE TABLE IF NOT EXISTS liste_couleurs (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nom VARCHAR(100) NOT NULL
+);
+
+-- 2. Insertion des noms uniquement
+INSERT INTO liste_couleurs (nom) VALUES
+('Abricot'), ('Acajou'), ('Aigue-marine'), ('Alizarine'), ('Amande'), 
+('Amarante'), ('Ambre'), ('Améthyste'), ('Anthracite'), ('Aquamarine'), 
+('Ardoise'), ('Argent'), ('Aubergine'), ('Aurore'), ('Avocat'), 
+('Azur'), ('Baillet'), ('Basane'), ('Beige'), ('Bistre'), 
+('Blanc'), ('Bleu'), ('Bleu acier'), ('Bleu ciel'), ('Bleu marine'), 
+('Bleu nuit'), ('Bleu pétrole'), ('Bleu roi'), ('Bordeaux'), ('Bourgogne'), 
+('Brique'), ('Bronze'), ('Brou de noix'), ('Brun'), ('Bureau'), 
+('Caca d''oie'), ('Cachou'), ('Cadmium'), ('Café'), ('Cannelle'), 
+('Capucine'), ('Caramel'), ('Carmin'), ('Carotte'), ('Céladon'), 
+('Cerise'), ('Chamois'), ('Chartreuse'), ('Châtain'), ('Chaudron'), 
+('Chocolat'), ('Cinabre'), ('Citrouille'), ('Coquelicot'), ('Corail'), 
+('Cramoisi'), ('Crème'), ('Cuisse de nymphe'), ('Cuivre'), ('Cyan'), 
+('Éburné'), ('Écarlate'), ('Écru'), ('Émeraude'), ('Fauve'), 
+('Flave'), ('Fraise'), ('Framboise'), ('Fuchsia'), ('Garance'), 
+('Glauque'), ('Glycine'), ('Grenat'), ('Gris'), ('Gris perle'), 
+('Gueules'), ('Héliotrope'), ('Incarnat'), ('Indigo'), ('Isabelle'), 
+('Ivoire'), ('Jaune'), ('Jaune canari'), ('Jaune citron'), ('Jaune d''or'), 
+('Kaki'), ('Lapis-lazuli'), ('Lavande'), ('Lie de vin'), ('Lilas'), 
+('Lin'), ('Magenta'), ('Maïs'), ('Malachite'), ('Mandarine'), 
+('Marron'), ('Mauve'), ('Menthe'), ('Moutarde'), ('Nacre'), 
+('Noir'), ('Noisette'), ('Ocre'), ('Olive'), ('Or'), 
+('Orange'), ('Orchidée'), ('Paille'), ('Parme'), ('Pêche'), 
+('Pervenche'), ('Plomb'), ('Poil de chameau'), ('Pourpre'), ('Prasin'), 
+('Prune'), ('Puce'), ('Rose'), ('Rouge'), ('Rubis'), 
+('Sable'), ('Safran'), ('Saphir'), ('Saumon'), ('Sépia'), 
+('Sinople'), ('Soufre'), ('Tabac'), ('Terracotta'), ('Tomate'), 
+('Topaze'), ('Turquoise'), ('Vermeil'), ('Vermillon'), ('Vert'), 
+('Vert bouteille'), ('Vert d''eau'), ('Violet'), ('Zinzolin');
